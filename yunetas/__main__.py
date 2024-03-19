4a3212b3c980064810139f2a1bdf8859572cdfa4
rmm <rosa.martinez@artgins.com>
2024-03-19 15:16:50 +0100

    now yunetas running

--- yunetas/__main__.py
+++
@@ -1,7 +0,0 @@
-import sys
-
-if __name__ == '__main__':
-    from yunetas import run
-
-    sys.exit(run())
-
---
+++ yunetas/my_venv.py
@@ -0,0 +1,28 @@
+import typer
+
+app = typer.Typer()
+
+
+@app.command()
+def create(venv_name: str):
+    print(f"Creating venv: {venv_name}")
+
+
+@app.command()
+def delete(venv_name: str):
+    print(f"Deleting venv: {venv_name}")
+
+
+@app.callback(invoke_without_command=True)
+def main(ctx: typer.Context):
+    """
+    Manage virtual environments
+    """
+    if ctx.invoked_subcommand is None:
+        # No subcommand was provided, so we print the help.
+        typer.main.get_command(app).get_help(ctx)
+        raise typer.Exit(code=1)
+
+
+if __name__ == "__main__":
+    app()
